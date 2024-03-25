c244439bd455d949e892e8f26bf93dd2e02c3ed9
Shane <shane.koester.contact@gmail.com>
2024-03-24 21:06:50 -0300

    commented unused CustomLink

--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import './NavStyles.css';
-import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
    const navbarLinksRef = useRef(null);
