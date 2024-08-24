ac2e36ed36183ecff9a293f0700d07eb747205b9
shane - laptop <shane.koester.contact@gmail.com>
2024-08-24 11:31:36 -0300

    added OCR demo

--- src/pages/projects/project-pages/PJ-OCR.jsx
+++ src/pages/projects/project-pages/PJ-OCR.jsx
@@ -1,14 +1,23 @@
-import React from 'react'; 
+import React from 'react';
 import { ProjectPage } from './ProjectPage';
 
 const images = [
    {
-      src: '',
+      src: 'https://imgur.com/1VDZ8FO.png',
+   },
+   {
+      src: 'https://imgur.com/x7OSIq4.png',
+   },
+   {
+      src: 'https://imgur.com/EU8ZuP8.png',
+   },
+   {
+      src: 'https://imgur.com/TRb7yby.png',
    },
 ]
 
 const PJOpticalCharacterRecognition = () => {
-   return <ProjectPage title='Optical Character Recognition' images={images}/>;
+   return <ProjectPage title='Optical Character Recognition' images={images} />;
 };
 
 export default PJOpticalCharacterRecognition;
